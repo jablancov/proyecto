6920d0f4fc75fd3eae90e88e4fb3aa8b7ebe91aa
Carlos-Nunez1504 <165734016+Carlos-Nunez1504@users.noreply.github.com>
2024-06-24 17:47:23 -0600

    Update tresenraya.c
    Se hacen cambios en el codigo .

--- parte_1/tresenraya.c
+++ parte_1/tresenraya.c
@@ -1,16 +1,34 @@
 #include <stido.h>
 #include <gtk/gtk.h>
-
+#define FLIEGLADE "tres-en-raya.glade"
 int turnosmaximos = 9;
+Gtkbuilder = *builder;
+Tresenraya = *tresenraya;
+Gerror = *err=null;
 
-void jugador1 (){
+typedef struct{
+ventanacasillasvacias = *ventanacasillasvacias;
+boton1vacio = *casilla1
+boton2vacio = *casilla2
+boton3vacio = *casilla3
+boton4vacio = *casilla4
+boton5vacio = *casilla5
+boton6vacio = *casilla6
+boton7vacio = *casilla7
+boton8vacio = *casilla8
+boton9vacio = *casilla9
+
+}
+int jugador1 (){
 
 }
 
-void jugador2 (){
+int jugador2 (){
     
 }
 
-int main(){
-    
-}
+void main(){
+    gtk
+    do{
+    } while (turnosmaximos <= 9);
+    }
