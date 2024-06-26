46311cccefd8d5f9cda39d47761ca09f76f592c1
Carlos-Nunez1504 <carlos.nunezvargas@ucr.ac.cr>
2024-06-26 10:59:54 -0600

    Update functions.c

--- parte_1/functions.c
+++ parte_1/functions.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include<stdbool.h>
 #include <gtk/gtk.h>
+#include<tres-en-rayafinal.glade>
 #include <functions_and_structs.h>
 
-
+int ventananueva {}[
+    window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+]
 int jugador1(){ /*este jugador va a jugar con O*/
 
 }
