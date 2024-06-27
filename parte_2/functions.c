5707913375990ce7cffb7f90640e93040a164503
Jorge Blanco <jorge.blancovasquez@ucr.ac.cr>
2024-06-27 00:40:18 -0600

    Se agrego la funcionalidad de calcular intervalos y mostrar la grafica acorde a estos

--- parte_2/functions.c
+++ parte_2/functions.c
@@ -1,46 +1,52 @@
 #include <stdio.h>
 #include "functions_and_structs.h"
+#include <math.h>
 
-float parabola(float a, float b, float c, float x) {
-    /*
-    Recibe los parametros para crear una funcion cuadratica, y retorna la imagen de esta para el valor de x dado.
-    */
-    return a*x*x+b*x+c;
-}
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
 
 
 
-int ask_func_params(cuad_func *fx, cuad_func *gx) {
-    /*Pregunta al usuario por los parametros de dos funciones cuadraticas y el error entre ellas
-    Guarda los parametros en los structs de dos funciones cuadraticas, f(x) y g(x)*/
+int ask_func_params(cuad_func *fx, cuad_func *gx, float *threshold_error) {
+    /* Pregunta al usuario por los parametros de dos funciones cuadraticas y el error entre ellas
+       Guarda los parametros en los structs de dos funciones cuadraticas, f(x) y g(x)
+       Retorna 0 si todos los datos se guardaron adecuadamente*/
+
+    int err = 0;
 
     printf("Defina el valor de a (float): ");
-    scanf("%f", &(fx->a));
+    err += scanf("%f", &(fx->a)) - 1; // C
     clean_buffer();
 
     printf("Defina el valor de b (float): ");
-    scanf("%f", &(fx->b));
+    err += scanf("%f", &(fx->c)) - 1;
     clean_buffer();
 
     printf("Defina el valor de c (float): ");
-    scanf("%f", &(fx->c));
+    err += scanf("%f", &(fx->c)) - 1;
     clean_buffer();
 
     printf("Ahora, para la función g(x):\n");
 
     printf("Defina el valor de d (float): ");
-    scanf("%f", &(gx->a));
+    err += scanf("%f", &(gx->a)) - 1;
     clean_buffer();
 
     printf("Defina el valor de e (float): ");
-    scanf("%f", &(gx->b));
+    err += scanf("%f", &(gx->b)) - 1;
     clean_buffer();
 
     printf("Defina el valor de h (float): ");
-    scanf("%f", &(gx->c));
+    err += scanf("%f", &(gx->c)) - 1;
     clean_buffer();
+
+    printf("Finalmente, indique el porcentaje de error entre las funciones:\n");
     
-    return 0;
+    printf("Defina el valor del error porcentual maximo E (float): ");
+    err += scanf("%f", threshold_error) - 1;
+    clean_buffer();
+
+    return err;
 }
 
 
@@ -52,31 +58,40 @@ void clean_buffer() {
 
 
 void calculate_y(cuad_func *func, float x) {
-    /*Calcula y almacena la preimagen y la imagen de la funcion
-     */
+    /* Calcula el valor de y. Almacena la preimagen y la imagen de la
+    funcion dentro del struct que recibe     */
+
     func->y = func->a * x * x + func->b * x + func->c;
+    func->y = round(func->y * 100) / 100;
 }
 
 
 
-int generate_data(cuad_func *fx, cuad_func *gx, float x_min, float x_max, const char *filename) {
+int generate_data(cuad_func *fx, cuad_func *gx, float x_min, float x_max, region2d **region_list, float threshold_err, const char *filename) {
     /*
-    Recibe las funciones fx, gx y calcula los valores de x, y desde x_min hasta x_max
-
+    A partir de fx, gx y calcula los puntos x, y desde x_min hasta x_max.
+    Tambien calcula las regiones donde la diferencia entre las funciones es menor al threshold.
     */
 
-    FILE *data_file = fopen(filename, "w");
-    float step = 0.1;
+    FILE *data_file = fopen(filename, "w"); // Archivo para escribir los datos
+    float step = 0.2;
     float x;
+    float x_prev;
+    float err_diff;
+    int is_first_point = 1; // Indica si se va a agregar el primer punto al intervalo
+    int region_index = 0; // Indica el numero de intervalo
+    region2d *region = NULL;
+
 
     if (data_file == NULL) {
         perror("Error al abrir/crear el archivo de datos para los puntos");
         return -1;
     }
     
-    fprintf(data_file, "#x f(x) g(x)\n"); // Header del archivo, para saber el orden 
+    fprintf(data_file, "#x f(x) g(x)\n"); // Header del archivo, para saber el orden, está comentado 
 
     x = x_min;
+
     while (x < x_max)
     {
         calculate_y(fx, x);
@@ -84,6 +99,49 @@ int generate_data(cuad_func *fx, cuad_func *gx, float x_min, float x_max, const 
         
         fprintf(data_file, "%.1f %.1f %.1f\n", x, fx->y, gx->y);
 
+
+        // Calculo del error evitando division por 0
+        if (fx->y != 0) {
+
+            err_diff = 100 * fabs(fx->y - gx->y) / fabs(fx->y);
+
+        } else {
+
+            err_diff = (gx->y == 0) ? 0 : 100;
+
+        }
+
+
+        if (err_diff < threshold_err) {
+            if (is_first_point) {
+                printf("\nSe detecto un primer intervalo\n");
+                region = region_list[region_index];
+                region->x_min = x;
+                region->y_min = MIN(fx->y, gx->y);
+                region->x_max = x;
+                region->y_max = MAX(fx->y, gx->y);
+                is_first_point = 0; // El siguiente punto no es el primero del intervalo
+            }
+            else if (region->x_max == x_prev)
+            {
+                region->x_max = x;
+                region->y_max = MAX(fx->y, gx->y);
+            }
+            else if (region_index < 1){
+                printf("\nSe detecto un segundo intervalo\n");
+                region_index = 1;
+                region = region_list[region_index]; // Se crea un nuevo intervalo
+                region->x_min = x;
+                region->y_min = MIN(fx->y, gx->y);
+                region->x_max = x;
+                region->y_max = MAX(fx->y, gx->y);
+            }
+            else {
+                printf("\nSe detecto mas de dos intervalos, solo se van a usar los primeros 2.\nSe detuvo la generacion de puntos\n");
+                return -1;
+            }
+        }
+        x_prev = x;
         x += step;
     }
 
@@ -93,13 +151,19 @@ int generate_data(cuad_func *fx, cuad_func *gx, float x_min, float x_max, const 
 }
 
 
-void create_gnuplot_script(const char *script_name, const char *data_file, cuad_func *fx, cuad_func *gx, interval interval) {
+void create_gnuplot_script(const char *script_name, const char *data_file, cuad_func *fx, cuad_func *gx, region2d **region_list) {
     /* Crea el script para poder elaborar una grafica adecuada en gnuplot.*/
 
     //Variables
     FILE *script = fopen(script_name, "w");
-    char fx_legend[30];
-    char gx_legend[30];
+    char fx_legend[50];
+    char gx_legend[50];
+    float plt_x0;
+    float plt_x1;
+    float plt_y0;
+    float plt_y1;
+    float int_range_x;
+    float int_range_y;
 
     // Definir leyendas
     sprintf(fx_legend, "f(x) = %.1fx^2 + %.1fx + %.1f", fx->a, fx->b, fx->c);
@@ -112,8 +176,53 @@ void create_gnuplot_script(const char *script_name, const char *data_file, cuad_
     fprintf(script, "plot '%s' using 1:2 with lines title \"%s\", ", data_file, fx_legend);
     fprintf(script, "'%s' using 1:3 with lines title \"%s\"\n", data_file, gx_legend);
 
-    // Hace una pausa indefinida para que la ventana con la grafica se mantenga hasta que se cierre manualmente
-    fprintf(script, "pause -1");
+    for (int i = 0; i < 2; i++) {
+    if (region_list[i]->x_min != region_list[i]->x_max) {
+        // Dibujar un cuadro para los intervalos
+        fprintf(script, "set object %d rect from %.2f,%.2f to %.2f,%.2f front fs empty lw 3\n", (i+1), region_list[i]->x_min, region_list[i]->y_min, region_list[i]->x_max, region_list[i]->y_max);
+    }
+    }
+
+
+    if ((region_list[0]->x_min == region_list[0]->x_max) && (region_list[1]->x_min == region_list[1]->x_max)) { // Si no hay ningun intervalo
+        // No hace nada, el intervalo por defecto.
+    }
+    else if (region_list[1]->x_min == region_list[1]->x_max) {  // Si solo hay un intervalo
+    
+        int_range_x = fabs(region_list[0]->x_max-region_list[0]->x_min);
+        int_range_y = fabs(region_list[0]->y_max-region_list[0]->y_min);
+
+        plt_x0 = region_list[0]->x_min - 0.4*int_range_x;
+        plt_x1 = region_list[0]->x_max + 0.4*int_range_x;
+        plt_y0 = MIN(region_list[0]->y_min, region_list[0]->y_max) - 0.4*int_range_y;
+        plt_y1 = MAX(region_list[0]->y_min, region_list[0]->y_max) + 0.4*int_range_y;
+
+        fprintf(script, "set xrange [%.2f:%.2f]\n", plt_x0, plt_x1);
+        fprintf(script, "set yrange [%.2f:%.2f]\n", plt_y0, plt_y1);
+    }
+    else
+    {
+        float y0_max = MAX(region_list[0]->y_max,region_list[0]->y_min);
+        float y0_min = MIN(region_list[0]->y_max,region_list[0]->y_min);
+        float y1_max = MAX(region_list[1]->y_max,region_list[1]->y_min);
+        float y1_min = MIN(region_list[1]->y_max,region_list[1]->y_min);
+
+        int_range_x = fabs(region_list[1]->x_max-region_list[0]->x_min);
+        int_range_y = fabs(MAX(y0_max, y1_max)-MIN(y0_min, y1_min));
+
+        plt_x0 = region_list[0]->x_min - 0.4*int_range_x;
+        plt_x1 = region_list[1]->x_max + 0.4*int_range_x;
+
+        plt_y0 = MIN(y0_min, y1_min) - 0.4*int_range_y;
+        plt_y1 = MAX(y0_max, y1_max) + 0.4*int_range_y;
+
+        fprintf(script, "set xrange [%.2f:%.2f]\n", plt_x0, plt_x1);
+        fprintf(script, "set yrange [%.2f:%.2f]\n", plt_y0, plt_y1);
+    }
+
+
+    // Se refresca la grafica y hace una pausa indefinida para que la ventana con la grafica se mantenga hasta que se cierre manualmente
+    fprintf(script, "refresh\npause -1");
 
     fclose(script);
 
--- parte_2/main.c
+++ parte_2/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include "functions_and_structs.h"
 
 int main() {
@@ -8,23 +9,53 @@ int main() {
     const char *data_file = "functions_plot.dat";
     const char *script_gp = "plot_script.gp";
     char polt_command[30];
+    float threshold_error;
+    int err_args = 0;
     cuad_func fx;
     cuad_func gx;
-    interval set_1;
+    region2d *region_list[2];
 
     print_instrucciones(instructions_filename);
 
-    ask_func_params(&fx, &gx);
+    do
+    {
+        err_args =  ask_func_params(&fx, &gx, &threshold_error);
+        if (err_args != 0) {
+            printf("Error al guardar los argumentos, vuelva a intentar\n");
+        }
+    } while (err_args != 0);
+    
+    
 
-    generate_data(&fx, &gx, -100000, 100000, data_file);
+    for (int i = 0; i < 2; i++) {
+        region_list[i] = malloc(sizeof(region2d));
+        if (region_list[i] == NULL) {
+            perror("Failed to allocate memory for region");
+            return -1;
+        }
+        region_list[i]->x_min = 0.0;
+        region_list[i]->x_max = 0.0;
+        region_list[i]->y_min = 0.0;
+        region_list[i]->y_max = 0.0;
+    }
 
-    /*Preguntar por la diferencia maxima*/
-    /*Calcular intervalo*/
 
-    /*Plotear*/
-    create_gnuplot_script(script_gp, data_file, &fx, &gx, set_1);
-    sprintf(polt_command, "gnuplot %s", script_gp);
-    system(polt_command);
+    generate_data(&fx, &gx, -100000, 100000, region_list, threshold_error, data_file);
+
+
+    create_gnuplot_script(script_gp, data_file, &fx, &gx, region_list);
+
+    printf("Intervalos\n");
+
+    for (int i = 0; i < 2; i++) {
+        printf("x_min: %.2f, x_max: %.2f, y_min: %.2f, y_max: %.2f\n", region_list[i]->x_min,region_list[i]->x_max,region_list[i]->y_min,region_list[i]->y_max);
+        free(region_list[i]);
+    }
+
+    sprintf(polt_command, "gnuplot %s", script_gp); // Crear un comando para realizar la grafica
+    system(polt_command); // Mostrar la grafica
+
+
 
     return 0;
 }
